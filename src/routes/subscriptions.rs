37e7ff3eefb02f38b98c36e936a50fe70ce43b17
abdulsamed1 <abdalsamed71@gmail.com>
2025-10-03 22:14:25 +0300

    Update dependencies and modify subscription route
    - Updated the `Cargo.toml` file to:
      - Change the edition from 2021 to 2025.
      - Add authorship information.
      - Update dependencies to their latest versions, including `actix-web`, `tokio`, `serde`, `config`, `sqlx`, `uuid`, and `chrono`.
      - Introduce `dev-dependencies` for `reqwest`.
    
    - Modified the `subscriptions` route in `src/routes/subscriptions.rs`:
      - Added SQL query to insert subscription data into the database.
      - Updated imports to include necessary traits for the new functionality.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, HttpResponse};
-use sqlx::PgConnection;
+use actix_web::{guard::Connect, web, HttpResponse};
+use sqlx::{Connection, PgConnection};
 /****************************************************************************************
  * spawn_app function to start the application server for testing
  * bind a TcpListener to an available port on localhost
@@ -13,6 +13,17 @@ pub async fn subscriptions(
     _form: web::Form<FormData>,
     _connection: web::Data<PgConnection>,
 ) -> HttpResponse {
-    
+    sqlx::query!(
+        r#"
+insert into subscriptions (id, email, username, subscribed_at)
+values ($1, $2, $3, $4)
+"#,
+        Uuid::new_v4(),
+        form._email,
+        form._username,
+        Utc::now()
+    )
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
